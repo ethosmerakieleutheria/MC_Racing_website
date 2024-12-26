faf878c33cbcdf7e167bdffe05a9861cb58c16b7
sanidhaya <sanidhaya08sharma11@gmail.com>
2024-12-26 18:26:45 -0500

    added backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+
+router = APIRouter()
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello, World!"}
+
+@app.get("mcRacing")
+async def mc_racing():
+    return {"message": "Welcome to the MC Racing World!"}
 
