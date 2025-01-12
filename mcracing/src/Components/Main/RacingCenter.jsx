9534d1382cb0c09f905c8b4c9957b4589a762811
Taleef Tamsal <89072337+Taleef7@users.noreply.github.com>
2025-01-12 03:13:38 -0500

    gitignore and env issues

--- mcracing/src/Components/Main/RacingCenter.jsx
+++ mcracing/src/Components/Main/RacingCenter.jsx
@@ -6,13 +6,14 @@ import { MapPin, Clock } from 'lucide-react';
 import React from 'react';
 
 const RacingCenter = () => {
+  const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   const location = {
     address: "701 S Clinton St Suite 709",
     city: "Fort Wayne, IN 46802",
     directionsLink:
       "https://www.google.com/maps/place/MC+Racing/@41.0802971,-85.1406338,17z/data=!3m2!4b1!5s0x8815e48d7df5393f:0xbb8126f7c6dd34b2!4m6!3m5!1s0x8815e59ff433cddd:0xd31403685da5f34a!8m2!3d41.0802971!4d-85.1380589!16s%2Fg%2F11y245lj75?entry=ttu&g_ep=EgoyMDI1MDEwOC4wIKXMDSoASAFQAw%3D%3D",
     staticMap:
-      "https://maps.googleapis.com/maps/api/staticmap?center=701+S+Clinton+St+Suite+709,Fort+Wayne,IN+46802&zoom=15&size=400x200&markers=color:red%7C701+S+Clinton+St+Suite+709,Fort+Wayne,IN+46802&key=AIzaSyD591OKuUaezgbZ-hT34ewtY6OW2NiFwcg"
+      `https://maps.googleapis.com/maps/api/staticmap?center=701+S+Clinton+St+Suite+709,Fort+Wayne,IN+46802&zoom=15&size=400x200&markers=color:red%7C701+S+Clinton+St+Suite+709,Fort+Wayne,IN+46802&key=${apiKey}`
   };
 
   const hours = [
